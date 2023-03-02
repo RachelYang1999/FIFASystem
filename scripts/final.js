0f0592af6cbebb10b4c9517d07e5904c622f1c7a
michael <xuzy8888@126.com>
2023-03-02 09:03:35 -0600

    match detail display

--- scripts/final.js
+++ scripts/final.js
@@ -17,17 +17,20 @@ function createGame(player1, hour, player2) {
 }
 
 let delay = -0.15
-function createCard(date, day, games, finished) {
+function createCard(date, day, time, games, finished, city, temperature, ticket) {
   delay = delay + 0.15
   return `
     <div class="card" style="animation-delay: ${delay}s">
-      <h2>${date} <span>${day}</span></h2>
+      <h2>${date} <span>${day}</span> <span>${time}</span></h2>
       <div class="finalizado">
         <h3>${finished}</h3>
       </div>
       <ul>
         ${games}
       </ul>
+      <h2>City: <span>${city}</span> </h2>
+      <h2>Temperature: <span>${temperature}</span> </h2>
+      <h2>Tickets Sold: <span>${ticket}</span> </h2>
     </div>
   `
 }
@@ -36,12 +39,20 @@ document.querySelector("#cards").innerHTML =
   createCard(
     "17/12",
     "Saturday",
+    "18:00",
     createGame("Croatia", "2 x 1", "Marroco"),
-    "3rd PLACE COMPETITION"
+    "3rd PLACE COMPETITION",
+      "Al Rayyan",
+      "73°F",
+      "9998"
   ) +
   createCard(
     "18/12",
     "SUNDAY",
+    "18:00",
     createGame("argentina", "3 <strong style='font-size: 20px'>(4 x 2)</strong> 3", "France"),
-    "final"
+    "final",
+      "Lusail",
+      "73°F",
+      "15079"
   )
